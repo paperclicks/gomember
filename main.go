25bf1e401deb62ae7e62bc13bf7363a43b035da4
Ervin Hoxha <ervin@paperclicks.net>
2018-10-10 22:10:01 +0200

    test gitignore

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"os"
-
-	"github.com/paperclicks/gomember/amember"
-)
-
 func main() {
 
-	//
-	am := amember.New("https://membership.theoptimizer.io", "HUuL1WiRgA3ISZZZyyzo", os.Stdout)
-
-	p := amember.Params{
-		Filter: map[string]string{"user_id": "8"},
-	}
-	//am.Users(p)
-	//am.Invoices(p)
-	am.Accesses(p)
-	//am.Payments(p)
 }
